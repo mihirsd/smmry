46b384f17a65a9255646e65a161f373eb9f694a7
Mihir Desai <mihir@mihirdesai.net>
2024-04-26 01:46:00 +0530

    Fix purgecss

--- app/templates/postcss.config.js
+++ app/templates/postcss.config.js
@@ -1,6 +1,8 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
   content: [
-    './src/*.js'
+    './src/*.js',
+    './src/components/*.js',
+    './src/components/views/*.js'
   ],
   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
 })
