4e5228fb2b3e049536705afbe9a5adf948b127c5
Mihir Desai <mihir@mihirdesai.net>
2024-04-26 01:46:01 +0530

    Update footer

--- app/templates/src/components/Footer.js
+++ app/templates/src/components/Footer.js
@@ -4,7 +4,7 @@ function Main() {
   return(
     <div className="Footer">
       <footer className="w-full p-4 text-center text-gray-600 tracking-wide">
-        <a href="https://github.com/mihir3k/smmry">GitHub</a>
+        <a href="https://github.com/mihir3k/smmry">Source</a>
       </footer>
     </div>
   );
