fe569f4eadcc8251f3972018f85e378dcb5dc517
Mihir Desai <mihir@mihirdesai.net>
2024-04-26 01:45:54 +0530

    Update flask routes

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 from app import config
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../templates', static_folder='../static')
 app.config.from_object(config.Config)
 
 from app import routes
--- api/app/routes.py
+++ api/app/routes.py
@@ -1,5 +1,11 @@
+from flask import render_template
+
 from app import app
 
-@app.route('/api')
+@app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/api')
+def api():
     return { 'message': 'Hello, World!' }
